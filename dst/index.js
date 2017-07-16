342081329c8538b1dc62dd176fda2e192ca9bb82
Joshua Wulf <josh@magikcraft.io>
2017-07-17 07:50:40 +1000

    spells -> loreToLoad

--- dst/index.js
+++ dst/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var aspecto = require("./spells/aspecto");
 var viburnum = require("./spells/viburnum");
 exports.description = "The core Magikcraft API spells";
-exports.spells = [
+exports.loreToLoad = [
     aspecto,
     viburnum
 ];
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as aspecto from './spells/aspecto';
 import * as viburnum from './spells/viburnum';
 export const description = "The core Magikcraft API spells";
 
-export const spells = [
+export const loreToLoad = [
     aspecto,
     viburnum
 ];
