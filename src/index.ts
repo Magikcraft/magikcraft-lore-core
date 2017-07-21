c7116808a583c754522cb3f89043d2f56b680b30
Timothy Marwick <triyuga@gmail.com>
2017-07-21 15:55:54 +1000

    [add] test123 lore

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 
 import * as aspecto from './spells/aspecto';
 import * as viburnum from './spells/viburnum';
+import * as test123 from './spells/test123';
 export const description = "The core Magikcraft API spells";
 
 export const loreToLoad = [
     aspecto,
-    viburnum
+    viburnum,
+    test123,
 ];
