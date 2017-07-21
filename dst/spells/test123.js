71dd1ea5210d1fdb165c3792ca02f85c67ec4f7f
The Legendary Josh Wulf™ - As Seen on the Internet® <josh@magikcraft.io>
2017-07-21 22:55:28 +1000

    update -> 789

--- dst/spells/test123.js
+++ dst/spells/test123.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.name = 'test123';
 exports.cost = 0;
 exports.code = function (canon) { return function () {
-    canon.magik.dixit('test456');
+    canon.magik.dixit('test789');
 }; };
--- src/spells/test123.ts
+++ src/spells/test123.ts
@@ -3,5 +3,5 @@ import { ICanon } from '../magikcraft-types/canon';
 export const name = 'test123';
 export const cost = 0;
 export const code = (canon: ICanon) => () => {
-	canon.magik.dixit('test456');
+	canon.magik.dixit('test789');
 };
