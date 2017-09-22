3d8b6b8546a1217e1def6258016dc75288db6e0e
Joshua Wulf <sitapati@worldsankirtan.net>
2017-09-22 19:19:47 +1000

    Add ICanon type

---
+++ src/magikcraft-types/Canon.d.ts
@@ -0,0 +1,15 @@
+declare interface ICanon {
+    _darkmagik_: any;
+    console: any;
+    constant: any;
+    displayLocalMsg: (msg: string, ...args: any[]) => void;
+    globalLocale: string;
+    http: any;
+    localiseMsg: (msg: string, ...args: any[]) => string;
+    log: any;
+    magik: any;
+    MSG: any;
+    msg: (msg_id: string) => void;
+    plugin: any;
+    sender: any;
+}
