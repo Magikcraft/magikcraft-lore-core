560806187c6205b8a43b48f512556a1a2843c4a5
Joshua Wulf <sitapati@worldsankirtan.net>
2017-10-05 21:26:41 +1000

    Fix exmemento default

--- src/lore/exmemento.ts
+++ src/lore/exmemento.ts
@@ -6,7 +6,7 @@ declare const global: any, magikcraft: any;
  */
 export const name = 'exmemento';
 export const cost = 0;
-export const code = (canon: ICanon) => (key = '__default'): any => {
+export const code = (canon: ICanon) => (key = 'memory.default'): any => {
     const magik = magikcraft.io;
     if (magik.durablePlayerMap.containsKey(key)) {
         return magik.durablePlayerMap.get(key);
